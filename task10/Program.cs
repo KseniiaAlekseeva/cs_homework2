b2e127256de5454686a22309632cf1c69e4126de
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-01-31 17:42:14 +0300

    Add task10

---
+++ task10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Program takes three-digit number as input 
+// and gives the second digit of this number as output.
+
+bool corr = false;
+int num = 0, secNum = 0;
+
+while (corr == false)
+{
+    Console.WriteLine("Enter a three-digit number: ");
+    num = int.Parse(Console.ReadLine());
+    if (num / 100 < 10)
+    {
+        secNum = Math.Abs((num % 100) / 10);
+        Console.WriteLine($"The second digit of number {num} is {secNum}: ");
+        corr = true;
+    }
+}
