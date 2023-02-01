442310b8e7e518d8985cd8dc26da1c0ad0b20fee
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-02-01 10:12:04 +0300

    Add task13

---
+++ task13/Program.cs
@@ -0,0 +1,26 @@
+﻿// Program gives the third digit of entered number
+// or informs that there is no third digit
+
+int num = 0;
+
+Console.WriteLine("Enter a number: ");
+num = int.Parse(Console.ReadLine());
+
+int curnum = Math.Abs(num);
+int quot = curnum;
+int thirdNum = 0;
+
+if (curnum < 100) Console.WriteLine($"Number {num} doesn't have third digit.");
+else
+{
+    do
+    {
+        curnum = quot;
+        quot = curnum / 10;
+    }
+    while (quot > 99);
+
+    thirdNum = curnum % 10;
+    Console.WriteLine($"The third digit of number {num} is {thirdNum}.");
+}
+
