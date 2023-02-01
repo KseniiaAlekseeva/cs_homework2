7781ef5d0c84be9c20690f0796700d8e44e0770b
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-02-01 10:22:54 +0300

    Add task15

---
+++ task15/Program.cs
@@ -0,0 +1,12 @@
+﻿// Program takes the number of the day of the week as input 
+// and checks if it's a holiday.  
+
+int num = 0;
+while ((num < 1) || (num > 7))
+{
+    Console.WriteLine("Enter the number of the day of the week: ");
+    num = int.Parse(Console.ReadLine());
+
+    if (num > 5) Console.WriteLine($"The day with number {num} is a holiday.");
+    else Console.WriteLine($"The day with number {num} is not a holiday.");
+}
